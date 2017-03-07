e43a4e96ee1102a5200fe9e928b3cfd6e69f5e6c
Marcio Gasparotto <mgasparotto@gmail.com>
2017-03-07 00:38:06 -0300

    wip

--- src/pages/PowerTable/PowerTablePage.js
+++ src/pages/PowerTable/PowerTablePage.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import {PowerTable} from 'apollo-11';
+import {PowerTable, PowerColumn} from 'apollo-11';
 
 class PowerTablePage extends React.Component {
 
+  printPrice(row) {
+    return `R$ ${row.price}`;
+  }
+
+  printTotal(row) {
+    return `R$ ${row.price * row.quantity}`;
+  }
 
   render() {
     return (
       <div>
-        <PowerTable dataUrl='http://localhost:3000/users' />
+        <PowerTable dataUrl='http://localhost:3000/users' itensInViewPort={15} rowHeight={39.466}>
+          <PowerColumn columnTitle='Codigo' dataKey='id' />
+          <PowerColumn columnTitle='Nome' dataKey='firstName' searchable />
+          <PowerColumn columnTitle='Papel' dataKey='papel' searchable />
+          <PowerColumn columnTitle='Descrição' dataKey='text' searchable />
+          <PowerColumn columnTitle='Endereço' dataKey='address' searchable />
+          <PowerColumn columnTitle='Preço' dataKey='price' formatter={this.printPrice} />
+          <PowerColumn columnTitle='Quantidade' dataKey='quantity' />
+          <PowerColumn columnTitle='Total' formatter={this.printTotal} />
+        </PowerTable>
       </div>
     );
   }
