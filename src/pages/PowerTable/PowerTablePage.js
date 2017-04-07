8e1aacdd4f677b18816e3b009559001872fc9f97
Marcio Gasparotto <mgasparotto@gmail.com>
2017-04-07 14:23:56 -0300

    wip

--- src/pages/PowerTable/PowerTablePage.js
+++ src/pages/PowerTable/PowerTablePage.js
@@ -7,6 +7,10 @@ class PowerTablePage extends React.Component {
     return `R$ ${row.price}`;
   }
 
+  printPriceOnFilter(price) {
+    return `R$ ${price}`;
+  }
+
   printTotal(row) {
     return `R$ ${parseFloat(row.price * row.quantity).toFixed(2)}`;
   }
@@ -20,7 +24,13 @@ class PowerTablePage extends React.Component {
           <PowerColumn columnTitle='Papel' dataKey='papel' searchable />
           <PowerColumn columnTitle='Descrição' dataKey='text' searchable />
           <PowerColumn columnTitle='Endereço' dataKey='address' searchable />
-          <PowerColumn columnTitle='Preço' dataKey='price' dataType='numeric' formatter={this.printPrice} searchable />
+          <PowerColumn
+            columnTitle='Preço'
+            dataKey='price'
+            dataType='numeric'
+            formatter={this.printPrice}
+            formatterOnFilter={this.printPriceOnFilter}
+            searchable />
           <PowerColumn columnTitle='Quantidade' dataKey='quantity' dataType='numeric' searchable />
           <PowerColumn columnTitle='Total' formatter={this.printTotal} />
         </PowerTable>
